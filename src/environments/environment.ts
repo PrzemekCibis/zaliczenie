d73561a0b6a073e601eb2862d0859caf38e57e9c
PrzemekCibis <przemek00wsb@gmail.com>
2021-07-02 13:14:58 +0200

    zaliczenie

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDOqXZMzwu4Kq32_WUAFZVqgNlu51-Rxkw",
+    authDomain: "przemek-30a0a.firebaseapp.com",
+    projectId: "przemek-30a0a",
+    storageBucket: "przemek-30a0a.appspot.com",
+    messagingSenderId: "729885346851",
+    appId: "1:729885346851:web:318b997ef9e3ff1d2af596",
+    measurementId: "G-GZB78D6ELX"
+  }
+};
+
